ee9a66af94a3d9b09e85aceee03ce72e25423949
HelloMizanur <mizanur.rahman0050@gmail.com>
2026-03-14 12:03:12 +0600

    LiveBioCord

--- MyFirstReact/src/App.jsx
+++ MyFirstReact/src/App.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import "./App.css";
-
+import Card from "./Card";
 function App() {
-  return <></>;
+  let [name, setName] = useState("");
+  let [job, setJob] = useState("");
+  return (
+    <>
+      <p>Name:</p>
+      <input
+        type="text"
+        value={name}
+        placeholder="Your Name"
+        onChange={(e) => {
+          setName(e.target.value.toUpperCase());
+        }}
+      />
+      <p>Job:</p>
+      <input
+        value={job}
+        type="text"
+        placeholder="Your Job"
+        onChange={(e) => {
+          setJob(e.target.value);
+        }}
+      />
+      <Card name={name} job={job} />
+      <button
+        onClick={() => {
+          (setJob(""), setName(""));
+        }}
+      >
+        Reset
+      </button>
+    </>
+  );
 }
 
 export default App;
---
+++ MyFirstReact/src/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({ name, job }) {
+  return (
+    <>
+      <h1>{name}</h1>
+      <h3>{job}</h3>
+    </>
+  );
+}
