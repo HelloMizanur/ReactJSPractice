6e8f7b4c1434b073ecc96a27783df766ad4fadda
HelloMizanur <mizanur.rahman0050@gmail.com>
2026-03-15 13:56:11 +0600

    Done

--- WorldTour/src/Components/Countries.jsx
+++ WorldTour/src/Components/Countries.jsx
@@ -1,16 +1,27 @@
 import Country from "./Country";
-import { use } from "react";
+import { use, useState } from "react";
 
 function Countries({ data }) {
+  const [visitedCountry, setVisitedCountry] = useState([]);
+  const countVisitedCountry = (country) => {
+    const newVistedCountry = [...visitedCountry, country];
+    setVisitedCountry(newVistedCountry);
+  };
   const CountryData = use(data);
   const countries = CountryData.countries;
 
   return (
     <>
       <h1 className="text-4xl text-center mt-10">All Countries</h1>
+      <h1 className="text-2xl text-center mt-10">{visitedCountry.length}</h1>
+
       <div className="grid grid-cols-3 gap-3 w-10/12 mx-auto py-10">
         {countries.map((country) => (
-          <Country key={country.ccn3.ccn3} country={country} />
+          <Country
+            countVisitedCountry={countVisitedCountry}
+            key={country.ccn3.ccn3}
+            country={country}
+          />
         ))}
       </div>
     </>
--- WorldTour/src/Components/Country.jsx
+++ WorldTour/src/Components/Country.jsx
@@ -1,10 +1,22 @@
-const Country = ({ country }) => {
-  console.log(country.flags.flags.png);
+import { useState } from "react";
+const Country = ({ country, countVisitedCountry }) => {
+  const [visited, setViseded] = useState(false);
+  const handleVisted = (country) => {
+    setViseded(!visited);
+    countVisitedCountry(country);
+  };
   return (
     <div className="border-2 border-amber-300 rounded p-4 flex flex-col justify-center items-center gap-5">
       <p className="text-2xl">{country.name.common}</p>
       <img src={country.flags.flags.png} alt="" />
-      <button>Visited</button>
+      <button
+        onClick={() => {
+          handleVisted(country);
+        }}
+        className={`font-bold rounded-2xl py-3 px-4 text-white ${visited ? "bg-green-500" : "bg-amber-600"}`}
+      >
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
